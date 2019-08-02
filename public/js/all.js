8224c8f294ac644fb412d067620b59dafe0c68a8
Sergey <sergey86@gmail.com>
2019-08-02 11:41:09 +0300

    Laptop update commit

---
+++ public/js/all.js
@@ -0,0 +1,251 @@
+
+/**
+* Theme: SimpleAdmin Admin Template
+* Author: Coderthemes
+* Morris Chart
+*/
+
+!function($) {
+    "use strict";
+
+    var Dashboard = function() {};
+
+    //creates line chart
+    Dashboard.prototype.createLineChart = function(element, data, xkey, ykeys, labels, opacity, Pfillcolor, Pstockcolor, lineColors) {
+        Morris.Line({
+          element: element,
+          data: data,
+          xkey: xkey,
+          ykeys: ykeys,
+          labels: labels,
+          fillOpacity: opacity,
+          pointFillColors: Pfillcolor,
+          pointStrokeColors: Pstockcolor,
+          behaveLikeLine: true,
+          gridLineColor: '#eef0f2',
+          hideHover: 'auto',
+          lineWidth: '3px',
+          pointSize: 0,
+          preUnits: '$',
+          resize: true, //defaulted to true
+          lineColors: lineColors
+        });
+    },
+
+    //creates Stacked chart
+    Dashboard.prototype.createStackedChart  = function(element, data, xkey, ykeys, labels, lineColors) {
+        Morris.Bar({
+            element: element,
+            data: data,
+            xkey: xkey,
+            ykeys: ykeys,
+            stacked: true,
+            labels: labels,
+            hideHover: 'auto',
+            resize: true, //defaulted to true
+            gridLineColor: '#eeeeee',
+            barColors: lineColors
+        });
+    },
+
+    Dashboard.prototype.init = function() {
+
+        //create line chart
+        var $data  = [
+             { y: '2008', a: 50, b: 0 },
+            { y: '2009', a: 75, b: 50 },
+            { y: '2010', a: 30, b: 80 },
+            { y: '2011', a: 50, b: 50 },
+            { y: '2012', a: 75, b: 10 },
+            { y: '2013', a: 50, b: 40 },
+            { y: '2014', a: 75, b: 50 },
+            { y: '2015', a: 100, b: 70 }
+          ];
+        this.createLineChart('dashboard-line-chart', $data, 'y', ['a', 'b'], ['Mobiles', 'Tablets'],['0.1'],['#ffffff'],['#999999'], ['#458bc4', '#23b195']);
+
+        //creating Stacked chart
+        var $stckedData  = [
+            { y: '2005', a: 45, b: 180 },
+            { y: '2006', a: 75,  b: 65 },
+            { y: '2007', a: 100, b: 90 },
+            { y: '2008', a: 75,  b: 65 },
+            { y: '2009', a: 100, b: 90 },
+            { y: '2010', a: 75,  b: 65 },
+            { y: '2011', a: 50,  b: 40 },
+            { y: '2012', a: 75,  b: 65 },
+            { y: '2013', a: 50,  b: 40 },
+            { y: '2014', a: 75,  b: 65 },
+            { y: '2015', a: 100, b: 90 }
+        ];
+        this.createStackedChart('dashboard-bar-stacked', $stckedData, 'y', ['a', 'b'], ['Series A', 'Series B'], ['#458bc4', '#ebeff2']);
+
+    },
+    //init
+    $.Dashboard = new Dashboard, $.Dashboard.Constructor = Dashboard
+}(window.jQuery),
+
+//initializing
+function($) {
+    "use strict";
+    $.Dashboard.init();
+}(window.jQuery);
+/**
+* Theme: Abstack - Bootstrap 4 Web App kit
+* Author: Coderthemes
+* Module/App: Core js
+*/
+
+
+/**
+ * Components
+ */
+!function($) {
+    "use strict";
+
+    var Components = function() {};
+
+    //initializing tooltip
+    Components.prototype.initTooltipPlugin = function() {
+        $.fn.tooltip && $('[data-toggle="tooltip"]').tooltip()
+    },
+
+    //initializing popover
+    Components.prototype.initPopoverPlugin = function() {
+        $.fn.popover && $('[data-toggle="popover"]').popover()
+    },
+
+    //initializing custom modal
+    Components.prototype.initCustomModalPlugin = function() {
+        $('[data-plugin="custommodal"]').on('click', function(e) {
+        	Custombox.open({
+                target: $(this).attr("href"),
+                effect: $(this).attr("data-animation"),
+                overlaySpeed: $(this).attr("data-overlaySpeed"),
+                overlayColor: $(this).attr("data-overlayColor")
+            });
+        	e.preventDefault();
+        });
+    },
+
+    //initializing Slimscroll
+    Components.prototype.initSlimScrollPlugin = function() {
+        //You can change the color of scroll bar here
+        $.fn.slimScroll &&  $(".slimscroll-alt").slimScroll({ position: 'right',size: "5px", color: '#98a6ad',wheelStep: 10});
+    },
+
+    //range slider
+    Components.prototype.initRangeSlider = function() {
+        $.fn.slider && $('[data-plugin="range-slider"]').slider({});
+    },
+
+    /* -------------
+     * Form related controls
+     */
+    //switch
+    Components.prototype.initSwitchery = function() {
+        $('[data-plugin="switchery"]').each(function (idx, obj) {
+            new Switchery($(this)[0], $(this).data());
+        });
+    },
+
+     Components.prototype.initKnob = function() {
+         $('[data-plugin="knob"]').each(function(idx, obj) {
+            $(this).knob();
+         });
+     },
+
+
+    //initilizing
+    Components.prototype.init = function() {
+        var $this = this;
+        this.initTooltipPlugin(),
+        this.initPopoverPlugin(),
+        this.initSlimScrollPlugin(),
+        this.initCustomModalPlugin(),
+        this.initRangeSlider(),
+        this.initSwitchery(),
+        this.initKnob()
+    },
+
+    $.Components = new Components, $.Components.Constructor = Components
+
+}(window.jQuery),
+    //initializing main application module
+function($) {
+    "use strict";
+    $.Components.init();
+}(window.jQuery);
+
+
+
+
+
+/**
+ * Theme: Abstack - Bootstrap 4 Web App kit
+ * Author: Coderthemes
+ * Module/App: Main Js
+ */
+
+
+(function ($) {
+
+    'use strict';
+
+    function initSlimscroll() {
+        $('.slimscroll').slimscroll({
+            height: 'auto',
+            position: 'right',
+            size: "8px",
+            color: '#9ea5ab'
+        });
+    }
+
+    function initMetisMenu() {
+        //metis menu
+        $("#side-menu").metisMenu();
+    }
+
+    function initLeftMenuCollapse() {
+        // Left menu collapse
+        $('.button-menu-mobile').on('click', function (event) {
+            event.preventDefault();
+            $("body").toggleClass("enlarged");
+        });
+    }
+
+    function initEnlarge() {
+        if ($(window).width() < 1025) {
+            $('body').addClass('enlarged');
+        } else {
+            $('body').removeClass('enlarged');
+        }
+    }
+
+    function initActiveMenu() {
+        // === following js will activate the menu in left side bar based on url ====
+        $("#sidebar-menu a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().addClass("in");
+                $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().parent().addClass("active");
+                $(this).parent().parent().parent().parent().addClass("in"); // add active to li of the current link
+                $(this).parent().parent().parent().parent().parent().addClass("active");
+            }
+        });
+    }
+
+    function init() {
+        initSlimscroll();
+        initMetisMenu();
+        initLeftMenuCollapse();
+        initEnlarge();
+        initActiveMenu();
+    }
+
+    init();
+
+})(jQuery)
+
